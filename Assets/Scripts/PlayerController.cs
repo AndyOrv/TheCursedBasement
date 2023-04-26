f6bea61a95e7f9ff6248482faefcbda04d17d114
Andrew O <61236243+AndyOrv@users.noreply.github.com>
2023-04-26 15:12:47 +0100

    First bulk of game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,218 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+-- Author: Andrew Orvis
+-- Description: A player controller class allowing capabilies to move a player in first person jump and sprint
+ */
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Transform playerCamera = null;
+    [SerializeField] float mouseSensitivity = 3.5f;
+    [SerializeField] float walkSpeed = 6.0f;
+    [SerializeField] float runSpeed = 12.0f;
+    [SerializeField] float gravity = -13.0f;
+    [SerializeField] [Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
+    [SerializeField] [Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
+
+    [SerializeField] private float runBuildUpSpeed;
+    [SerializeField] private KeyCode runKey;
+
+    private float movementSpeed;
+
+    [SerializeField] private float slopeForce;
+    [SerializeField] private float slopeForceRayLength;
+
+    [SerializeField] bool lockCursor = true;
+
+    float cameraPitch = 0.0f;
+    float velocityY = 0.0f;
+    CharacterController controller = null;
+
+    Vector2 currentDir = Vector2.zero;
+    Vector2 currentDirVelocity = Vector2.zero;
+
+    Vector2 currentMouseDelta = Vector2.zero;
+    Vector2 currentMouseDeltaVelocity = Vector2.zero;
+
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+
+    [SerializeField] GameObject deathScreen;
+
+    private bool isJumping;
+    public bool isDead = false;
+
+    public bool Paused = false;
+
+    void Start()
+    {
+
+        controller = GetComponent<CharacterController>();
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        deathScreen.SetActive(false);
+    }
+
+    void Update()
+    {
+        cursorLock();
+
+        if (!isDead && !Paused) //if player isnt dead and game isnt paused allow for movement
+        {
+            UpdateMouseLook();
+            UpdateMovement();
+        }
+
+
+        if (isDead) { Respawn(); }
+    }
+
+    #region cursor and mouse
+    void UpdateMouseLook()
+    {
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
+
+        cameraPitch -= currentMouseDelta.y * mouseSensitivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
+
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+        transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
+    }
+    void cursorLock()
+    {
+        if (!Paused)
+        {
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+    }
+    #endregion
+
+    #region movement
+    // apply forces in correct direction for player movement
+    void UpdateMovement()
+    {
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetDir.Normalize();
+
+        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
+
+        if (controller.isGrounded)
+            velocityY = 0.0f;
+
+        velocityY += gravity * Time.deltaTime;
+
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * movementSpeed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if ((currentDir.x != 0 || currentDir.y != 0) && OnSlope())
+            controller.Move(Vector3.down * controller.height / 2 * slopeForce * Time.deltaTime);
+
+        SetMovementSpeed();
+        JumpInput();
+
+    }
+
+    private void SetMovementSpeed()
+    {
+        if (Input.GetKey(runKey))
+            movementSpeed = Mathf.Lerp(movementSpeed, runSpeed, Time.deltaTime * runBuildUpSpeed);
+        else
+            movementSpeed = Mathf.Lerp(movementSpeed, walkSpeed, Time.deltaTime * runBuildUpSpeed);
+    }
+    #endregion
+
+    #region jump code
+    private void JumpInput()
+    {
+        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    //applies collision flags for plyaer to hit off roofs and works through animation curve
+    private IEnumerator JumpEvent()
+    {
+        controller.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            controller.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!controller.isGrounded && controller.collisionFlags != CollisionFlags.Above);
+
+        controller.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+
+    //Applies additonal force to player to allow for smooth decent on slopes as gravity alone wont over come movement speed and causes player to jitter
+    private bool OnSlope()
+    {
+        if (isJumping)
+            return false;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, controller.height / 2 * slopeForceRayLength))
+            if (hit.normal != Vector3.up)
+                return true;
+        return false;
+    }
+    #endregion
+
+    #region killing player
+    public void killPlayer()
+    {
+        isDead = true;
+        deathScreen.SetActive(true);
+
+        //On death move camera to ground as if player has fallen over
+        //playerCamera.transform.localPosition = new Vector3(playerCamera.transform.localPosition.x, playerCamera.transform.localPosition.y - 1.5f, playerCamera.transform.localPosition.z);
+        //playerCamera.transform.localRotation = Quaternion.Euler(playerCamera.rotation.x, playerCamera.rotation.y, 45);
+    }
+
+    public void Respawn()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (isDead)
+            {
+                ellipse.GetComponent<GameObject>();
+                deathScreen.SetActive(false);
+            }
+        }
+    }
+    #endregion
+
+
+
+
+
+}
